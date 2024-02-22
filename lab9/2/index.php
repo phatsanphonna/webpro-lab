e826dadc87f64917ec2594d479c2b0d420f0fdc6
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2024-02-22 11:05:49 +0700

    Feat: Lab 9

--- lab9/2/index.php
+++ lab9/2/index.php
@@ -13,13 +13,20 @@
     <?php
     include "../conn.php";
 
-    if (isset($_POST['course_id']) && $http_response_header['REQUEST_METHOD'] == "POST") {
-      $sql = "UPDATE course SET title = '" . $_POST['title'] . "', dept_name = '" . $_POST['dept_name'] . "', credit = '" . $_POST['credit'] . "' WHERE course_id = '" . $_POST['course_id'] . "'";
+    if (isset($_POST['submit'])) {
+      $sql = "UPDATE course SET title = '" . $_POST['title'] . "', dept_name = '" . $_POST['dept_name'] . "', credit = '" . $_POST['credit'] . "' WHERE course_id = '" . $_POST['id'] . "'";
       $query = mysqli_query($conn, $sql);
     }
 
-    if (isset($_GET['id'])) {
-      $sql = "SELECT * FROM course WHERE course_id = '" . $_GET['id'] . "'";
+    if (isset($_POST['delete'])) {
+      $sql = "DELETE FROM course WHERE course_id = '" . $_POST['id'] . "'";
+      $query = mysqli_query($conn, $sql);
+    }
+
+    if (isset($_GET['id']) || isset($_POST['id'])) {
+      $course_id = isset($_GET['id']) ? $_GET['id'] : $_POST['id'];
+
+      $sql = "SELECT * FROM course WHERE course_id = '" . $course_id . "'";
       $query = mysqli_query($conn, $sql);
 
       if (mysqli_num_rows($query) > 0) {
@@ -30,34 +37,34 @@
           echo "<div class='field'>";
           echo "<label class='label'>Course ID</label>";
           echo "<div class='control'>";
-          echo "<input class='input' disabled type='text' placeholder='Course ID' value='" . $row['course_id'] . "'>";
+          echo "<input class='input' name='id' readonly type='text' placeholder='Course ID' value='" . $row['course_id'] . "'>";
           echo "</div>";
           echo "</div>";
 
           echo "<div class='field'>";
           echo "<label class='label'>Title</label>";
           echo "<div class='control'>";
-          echo "<input class='input' type='text' placeholder='Title' value='" . $row['title'] . "'>";
+          echo "<input class='input' name='title' type='text' placeholder='Title' value='" . $row['title'] . "'>";
           echo "</div>";
           echo "</div>";
 
           echo "<div class='field'>";
           echo "<label class='label'>Dept. Name</label>";
           echo "<div class='control'>";
-          echo "<input class='input' type='text' placeholder='Dept. Name' value='" . $row['dept_name'] . "'>";
+          echo "<input class='input' name='dept_name' type='text' placeholder='Dept. Name' value='" . $row['dept_name'] . "'>";
           echo "</div>";
           echo "</div>";
 
           echo "<div class='field'>";
           echo "<label class='label'>Credit</label>";
           echo "<div class='control'>";
-          echo "<input class='input' type='number' placeholder='Credit' value='" . $row['credit'] . "'>";
+          echo "<input class='input' name='credit' type='number' placeholder='Credit' value='" . $row['credit'] . "'>";
           echo "</div>";
           echo "</div>";
 
           echo "<div class='field'>";
-          echo "<button type='submit' class='button is-primary'>Update</button>";
-          echo "<button type ='reset' class='button mx-2 is-danger'>Reset</button>";
+          echo "<button type='submit' name='submit' class='button is-primary mr-2'>Update</button>";
+          echo "<button type='submit' name='delete' class='button is-danger'>Delete</button>";
           echo "</div>";
 
           echo "</form>";
